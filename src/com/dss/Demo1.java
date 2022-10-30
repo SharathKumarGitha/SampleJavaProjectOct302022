85ac5fe7c64ed205dc2b41a9ae141c8a4070b3d5
sharathgitha627 <sharathgitha1627@gmail.com>
2022-10-30 13:28:49 +0530

    first code...

---
+++ src/com/dss/Demo1.java
@@ -0,0 +1,13 @@
+package com.dss;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is sample java App");		
+		System.out.println("this is second line ");
+		System.out.println("this is third line");
+		System.out.println("this is fourth line");
+	}
+
+}
---
+++ src/com/dss/demo2.java
@@ -0,0 +1,9 @@
+package com.dss;
+
+public class demo2 {
+public static void main(String[] args) {
+	
+	System.out.println("this is first line");
+	System.out.println("this is second line.....");
+}
+}
