04a73d286dae4fcbcd67d9efecaf8d0341ec11b8
sharathgitha627 <sharathgitha1627@gmail.com>
2022-10-30 18:42:51 +0530

    code added...

--- src/com/dss/demo2.java
+++ src/com/dss/demo2.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("this is first line");
 	System.out.println("this is second line.....");
 	System.out.println("this is third line....");
-	System.out.println("something.....");
-	System.out.println("something2.....");
+	System.out.println("something5.....");
+	System.out.println("something6.....");
 }
 }
