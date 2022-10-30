47471cbfa544aff239bb3715f68bae3fc8129ef7
sharathgitha627 <sharathgitha1627@gmail.com>
2022-10-30 14:44:08 +0530

    third line added in the demo2 class

--- src/com/dss/demo2.java
+++ src/com/dss/demo2.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	
 	System.out.println("this is first line");
 	System.out.println("this is second line.....");
+	System.out.println("this is third line....");
 }
 }
