bb00dad20a6eff1fc992aabd596c4d550ecaff06
sharathgitha627 <sharathgitha1627@gmail.com>
2022-11-05 14:05:52 +0530

    if condition is added...

--- src/com/dss/demo2.java
+++ src/com/dss/demo2.java
@@ -6,6 +6,18 @@ public static void main(String[] args) {
 	System.out.println("this is first line");
 	System.out.println("this is second line.....");
 	System.out.println("this is third line....");
+	
+	
+	System.out.println("=============================");
+	
+	if(true)
+	{
+		System.out.println("this is true");
+	}
+	else
+	{
+		System.out.println("this is false");
+	}
 
 	System.out.println("something5.....");
 	System.out.println("something5.5");
